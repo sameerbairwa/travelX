f293cacf83ba1e8505be8d2afad9c64f0e3ccb34
sameerbairwa <sameerbairwa07@gmail.com>
2020-02-21 23:12:21 +0530

    update wather and days function

--- weather_days.py
+++ weather_days.py
@@ -11,3 +11,170 @@ weather = {'Foggy':1,
                'Heavy Rain':11}                
 days = {'Monday':1,'Tuesday':2,'Wednesday':3,'Thursday':4,'Friday':5,'Saturday':6,'Sunday':7}
 #weather fetch using darksky api
+
+def fn_w(wth,time):
+    l1=[0,0]
+    if(wth=='Foggy'):
+        l1[0]=time-(.05*time)
+        l1[1]=time+(.3*time)
+
+    elif(wth=='Partly Cloudy'):
+        l1[0]=time-(.1*time)
+        l1[1]=time+(.05*time)
+    elif(wth=='Clear'):
+        l1[0]=time-(0.1*time)
+        l1[1]=time+(0.1*time)
+    elif(wth=='Humid and Mostly Cloudly'):
+        l1[0]=time-(.05*time)
+        l1[1]=time+(.1*time)
+    elif(wth=='Humid and Foggy'):
+        l1[0]=time-(.03*time)
+        l1[1]=time+(.35*time)
+    elif(wth=='Mostly Cloudy'):
+        l1[0]=time-(.05*time)
+        l1[1]=time+(.07*time)
+        
+    elif(wth=='Humid'):
+        l1[0]=time-(.05*time)
+        l1[1]=time+(.05*time)
+        
+    elif(wth=='Humid and Partly Cloudy'):
+        l1[0]=time-(.1*time)
+        l1[1]=time+(.07*time)
+        
+    elif(wth=='Drizzle'):
+        l1[0]=time-(.05*time)
+        l1[1]=time+(.1*time)
+    elif(wth=='Light Rain'):
+        l1[0]=time-(.05*time)
+        l1[1]=time+(.15*time)
+    elif(wth=='Heavy Rain'):
+        l1[0]=time-(.3*time)
+        l1[1]=time+(.3*time)
+
+    #print(l1[0])
+    #print(l1[1])
+    return l1[1],l1[0]
+
+
+def fn_ti(time_interval,time,day):
+    l2=[0,0]
+    #print(time_interval)
+    #print(time)
+    if(day=='Monday'):
+        if(time_interval>=0 and time_interval<=8):
+            l2[0]=time-(.7*time)
+            l2[1]=time
+        elif(time_interval>8 and time_interval<=11):
+             l2[0]=time-(.1*time)
+             l2[1]=time+(.4*time)
+        elif(time_interval>11 and time_interval<=17):
+            l2[0]=time-(.1*time)
+            l2[1]=time+(.2*time)
+        elif(time_interval>17 and time_interval<=21):
+            l2[0]=time-(.15*time)
+            l2[1]=time+(.3*time)
+        elif(time_interval>21 and time_interval<=24):
+            l2[0]=time-(.2*time)
+            l2[1]=time+(.2*time)
+    elif(day=='Tuesday'):
+        if(time_interval>=00 and time_interval<=8):
+            l2[0]=time-(.7*time)
+            l2[1]=time
+        elif(time_interval>8 and time_interval<=11):
+             l2[0]=time-(.1*time)
+             l2[1]=time+(.4*time)
+        elif(time_interval>11 and time_interval<=17):
+            l2[0]=time-(.1*time)
+            l2[1]=time+(.2*time)
+        elif(time_interval>17 and time_interval<=21):
+            l2[0]=time-(.15*time)
+            l2[1]=time+(.3*time)
+        elif(time_interval>21 and time_interval<=24):
+            l2[0]=time-(.2*time)
+            l2[1]=time+(.2*time)
+    elif(day=='Wednesday'):
+        if(time_interval>=00 and time_interval<=8):
+            l2[0]=time-(.7*time)
+            l2[1]=time
+        elif(time_interval>8 and time_interval<=11):
+             l2[0]=time-(.1*time)
+             l2[1]=time+(.4*time)
+        elif(time_interval>11 and time_interval<=17):
+            l2[0]=time-(.1*time)
+            l2[1]=time+(.2*time)
+        elif(time_interval>17 and time_interval<=21):
+            l2[0]=time-(.15*time)
+            l2[1]=time+(.3*time)
+        elif(time_interval>21 and time_interval<=24):
+            l2[0]=time-(.2*time)
+            l2[1]=time+(.2*time)
+    elif(day=='Thursday'):
+        if(time_interval>=00 and time_interval<=8):
+            l2[0]=time-(.7*time)
+            l2[1]=time
+        elif(time_interval>8 and time_interval<=11):
+             l2[0]=time-(.1*time)
+             l2[1]=time+(.4*time)
+        elif(time_interval>11 and time_interval<=17):
+            l2[0]=time-(.1*time)
+            l2[1]=time+(.2*time)
+        elif(time_interval>17 and time_interval<=21):
+            l2[0]=time-(.15*time)
+            l2[1]=time+(.3*time)
+        elif(time_interval>21 and time_interval<=24):
+            l2[0]=time-(.2*time)
+            l2[1]=time+(.2*time)
+    elif(day=='Friday'):
+        if(time_interval>=00 and time_interval<=8):
+            l2[0]=time-(.7*time)
+            l2[1]=time
+        elif(time_interval>8 and time_interval<=11):
+             l2[0]=time-(.1*time)
+             l2[1]=time+(.4*time)
+        elif(time_interval>11 and time_interval<=17):
+            l2[0]=time-(.1*time)
+            l2[1]=time+(.2*time)
+        elif(time_interval>17 and time_interval<=21):
+            l2[0]=time-(.15*time)
+            l2[1]=time+(.3*time)
+        elif(time_interval>21 and time_interval<=24):
+            l2[0]=time-(.2*time)
+            l2[1]=time+(.2*time)
+    elif(day=='Saturday'):
+        if(time_interval>=00 and time_interval<=8):
+            l2[0]=time-(.7*time)
+            l2[1]=time
+        elif(time_interval>8 and time_interval<=11):
+             l2[0]=time-(.3*time)
+             l2[1]=time
+        elif(time_interval>11 and time_interval<=18):
+            l2[0]=time-(.3*time)
+            l2[1]=time
+        elif(time_interval>18 and time_interval<=21):
+            l2[0]=time
+            l2[1]=time+(.2*time)
+        elif(time_interval>21 and time_interval<=24):
+            l2[0]=time-(.3*time)
+            l2[1]=time
+    elif(day=='Sunday'):
+        if(time_interval>=00 and time_interval<=8):
+            l2[0]=time-(.7*time)
+            l2[1]=time
+        elif(time_interval>8 and time_interval<=11):
+             l2[0]=time-(.3*time)
+             l2[1]=time
+        elif(time_interval>11 and time_interval<=18):
+            l2[0]=time-(.3*time)
+            l2[1]=time
+        elif(time_interval>18 and time_interval<=21):
+            l2[0]=time
+            l2[1]=time+(.2*time)
+        elif(time_interval>21 and time_interval<=24):
+            l2[0]=time-(.3*time)
+            l2[1]=time
+
+    #print(l2[0])
+    #print(l2[1])
+            
+    return l2[1],l2[0]
