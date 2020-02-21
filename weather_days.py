0c2720f255a9360064b358d7e4d7144e79d872c9
sameerbairwa <sameerbairwa07@gmail.com>
2020-02-21 22:00:43 +0530

    created a new file for store weather and days details

---
+++ weather_days.py
@@ -0,0 +1,13 @@
+weather = {'Foggy':1,
+               'Partly Cloudy':2,
+               'Clear':3,
+               'Mostly Cloudy':4,
+               'Humid and Mostly Cloudy':5,
+               'Humid and Foggy':6,
+               'Humid':7,
+               'Humid and Partly Cloudy':8,
+               'Drizzle':9,
+               'Light Rain':10,
+               'Heavy Rain':11}                
+days = {'Monday':1,'Tuesday':2,'Wednesday':3,'Thursday':4,'Friday':5,'Saturday':6,'Sunday':7}
+#weather fetch using darksky api
