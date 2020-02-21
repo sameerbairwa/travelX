91fd3149354e2ac3f84ea316d5b81e0f0bdb73b4
sameerbairwa <sameerbairwa07@gmail.com>
2020-02-21 20:47:02 +0530

    updated code

--- ml-1.py
+++ ml-1.py
@@ -10,12 +10,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import xlrd
 import csv
-
+    
 import pandas as pd
 from pandas import DataFrame
 import matplotlib.pyplot as plt
 from sklearn import linear_model
-
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
